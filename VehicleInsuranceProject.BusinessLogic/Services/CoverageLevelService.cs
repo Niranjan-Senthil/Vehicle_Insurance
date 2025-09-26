d4cf0ce512c1a3bb3c439ba82b414e1e80b2befc
Varalakshmi <2438658@cognizant.com>
2025-09-26 23:09:44 +0530

    policy files

---
+++ VehicleInsuranceProject.BusinessLogic/Services/CoverageLevelService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using VehicleInsuranceProject.Repository.Models;
+using VehicleInsuranceProject.Repository.Repositories;
+
+namespace VehicleInsuranceProject.BusinessLogic.Services
+{
+    public class CoverageLevelService : ICoverageLevelService
+    {
+        private readonly ICoverageLevelRepository _coverageLevelRepository;
+
+        public CoverageLevelService(ICoverageLevelRepository coverageLevelRepository)
+        {
+            _coverageLevelRepository = coverageLevelRepository;
+        }
+
+        public async Task<IEnumerable<CoverageLevel>> GetAllCoverageLevelsAsync()
+        {
+            return await _coverageLevelRepository.GetAllCoverageLevelsAsync();
+        }
+
+        public async Task<CoverageLevel?> GetCoverageLevelByIdAsync(int id)
+        {
+            return await _coverageLevelRepository.GetCoverageLevelByIdAsync(id);
+        }
+
+        public async Task AddCoverageLevelAsync(CoverageLevel coverageLevel)
+        {
+            await _coverageLevelRepository.AddCoverageLevelAsync(coverageLevel);
+        }
+
+        public async Task UpdateCoverageLevelAsync(CoverageLevel coverageLevel)
+        {
+            await _coverageLevelRepository.UpdateCoverageLevelAsync(coverageLevel);
+        }
+
+        public async Task DeleteCoverageLevelAsync(int id)
+        {
+            await _coverageLevelRepository.DeleteCoverageLevelAsync(id);
+        }
+    }
+}
--- VehicleInsuranceProject/obj/Release/net8.0/VehicleInsuranceProject.AssemblyInfo.cs
+++ VehicleInsuranceProject/obj/Release/net8.0/VehicleInsuranceProject.AssemblyInfo.cs
@@ -12,13 +12,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehicleInsuranceProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-=======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b53eb563d2243690a810b1419c06c8c7574dfe")]
->>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("VehicleInsuranceProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehicleInsuranceProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
